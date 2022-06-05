a0095302c999c126e0b90090f5bc8b33be48152e
Brian Chang <brianc@fobi.ai>
2022-06-05 09:06:06 -0700

    Use new movie component to show a movie

--- src/app/movie-list/movie/movie.component.ts
+++ src/app/movie-list/movie/movie.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.scss']
+  styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent implements OnInit {
+  private _title: string = '';
+  private _year: string = '';
+  private _posterUrl: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get title() {
+    return this._title;
+  }
+  @Input() set title(value: string) {
+    this._title = value;
   }
 
+  get year() {
+    return this._year;
+  }
+  @Input() set year(value: string) {
+    this._year = value;
+  }
+
+  get posterUrl() {
+    return this._posterUrl;
+  }
+  @Input() set posterUrl(value: string) {
+    this._posterUrl = value;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
