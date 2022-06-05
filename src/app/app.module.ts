55fdcf7b9162dbeb25336430012b6d9e8a98a5ff
Brian Chang <brianc@fobi.ai>
2022-06-05 10:00:34 -0700

    Add an input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieComponent } from './movie-list/movie/movie.component';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
@@ -22,6 +24,9 @@ import { MovieSearchComponent } from './movie-search/movie-search.component';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movie-search',
   templateUrl: './movie-search.component.html',
-  styleUrls: ['./movie-search.component.scss']
+  styleUrls: ['./movie-search.component.scss'],
 })
 export class MovieSearchComponent implements OnInit {
+  private _searchTerm: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get searchTerm() {
+    return this._searchTerm;
   }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+  }
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  emitSearchEvent(event: KeyboardEvent) {
+    // TODO: if event is "enter", emit search term to parent then pass to list to perform search
+  }
 }
