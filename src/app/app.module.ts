b53b1556e8f40befc2442b6777b364ffcd75b7c1
Brian Chang <brianc@fobi.ai>
2022-06-05 08:14:49 -0700

    Generate movie and movie list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieComponent } from './movie-list/movie/movie.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, MovieListComponent, MovieComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
