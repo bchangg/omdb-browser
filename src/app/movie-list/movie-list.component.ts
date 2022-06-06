0ae2aa1137ea9f9a642923ba777593a6088411c5
Brian Chang <brianc@fobi.ai>
2022-06-06 12:31:42 -0700

    Use new OMDB service to retrieve temp list of movies

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OMDBResponse } from '../omdb.model';
+import { OMDBService } from '../omdb.service';
+import { Movie } from './movie/movie.model';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss']
+  styleUrls: ['./movie-list.component.scss'],
 })
 export class MovieListComponent implements OnInit {
+  private _movies: Array<Movie> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get movies() {
+    return this._movies;
+  }
+  set movies(value: Array<Movie>) {
+    this._movies = value;
   }
 
+  constructor(private omdb: OMDBService) {}
+
+  async ngOnInit(): Promise<void> {
+    this.movies = (await this.omdb.searchByName('spider')).Search;
+  }
 }
